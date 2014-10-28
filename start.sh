37e6c53ab95587871db606030823774fb82fdc63
unknown <bibin.w@LP-3C970EE1AB15.HCLC.CORP.HCL.IN>
2014-10-28 15:56:02 +0530

    docker file and start script

--- start.sh
+++ start.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
-
+# This script starts the database server.
+echo "Creating user $user for databases loaded from $url"
+# Now the provided user credentials are added
 /usr/sbin/mysqld &
 sleep 5
 echo "Creating user"
-echo "CREATE USER '$user' IDENTIFIED BY '$password'" | mysql --default-characte$
-echo "REVOKE ALL PRIVILEGES ON *.* FROM '$user'@'%'; FLUSH PRIVILEGES" | mysql $
-echo "GRANT SELECT ON *.* TO '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-c$
-if [ "$access" = "WRITE" ]; then
-echo "Granting Privileges"
-echo "GRANT ALL PRIVILEGES ON *.* TO '$user'@'%' WITH GRANT OPTION; FLUSH PRIVI$
+echo "CREATE USER '$user' IDENTIFIED BY '$password'" | mysql --default-character-set=utf8
+echo "REVOKE ALL PRIVILEGES ON *.* FROM '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
+echo "GRANT SELECT ON *.* TO '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
+echo "finished"
+if [ "$right" = "WRITE" ]; then
+echo "adding write access"
+echo "GRANT ALL PRIVILEGES ON *.* TO '$user'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
 fi
+# And we restart the server to go operational
 mysqladmin shutdown
 echo "Starting MySQL Server"
+/usr/sbin/mysqld
