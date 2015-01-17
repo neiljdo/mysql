cd5ef2a5535d04c51e277442374be50a8e824700
unknown <bibin.w@LP-3C970EE1AB15.HCLC.CORP.HCL.IN>
2015-01-17 10:55:45 +0530

    error correction

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ echo "CREATE USER '$user' IDENTIFIED BY '$password'" | mysql --default-character
 echo "REVOKE ALL PRIVILEGES ON *.* FROM '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
 echo "GRANT SELECT ON *.* TO '$user'@'%'; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
 echo "finished"
+echo "CREATE DATABASE wordpress" | mysql --default-character-set=utf8
+
 if [ "$right" = "WRITE" ]; then
 echo "adding write access"
 echo "GRANT ALL PRIVILEGES ON *.* TO '$user'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql --default-character-set=utf8
